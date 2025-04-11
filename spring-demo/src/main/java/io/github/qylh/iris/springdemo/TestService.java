00e6f380b29a8ff4aee0c48d892aa1c106cec61d
mahaotian's debian <maht2001@163.com>
2025-04-11 21:15:04 +0800

    Added service Spring boot demo

---
+++ spring-demo/src/main/java/io/github/qylh/iris/springdemo/TestService.java
@@ -0,0 +1,7 @@
+package io.github.qylh.iris.springdemo;
+
+public interface TestService {
+
+    public String test(Integer a);
+
+}
---
+++ spring-demo/src/test/java/io/github/qylh/iris/springdemo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.qylh.iris.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
